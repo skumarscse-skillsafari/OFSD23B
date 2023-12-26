f39f5c845c3c179c35ec450443a931ea30ce754f
skumarscse-skillsafari <sathish.kumar@skillsafari.in>
2023-12-26 12:26:45 +0530

    products-app-hosting

--- 5-Backend/1-Node/4-Third-party-modules/2-Express/4-HTTP-Methods/Client/script.js
+++ 5-Backend/1-Node/4-Third-party-modules/2-Express/4-HTTP-Methods/Client/script.js
@@ -2,15 +2,18 @@ const submit = document.querySelector("#submit");
 
 submit.addEventListener("click", async (e) => {
   e.preventDefault();
-  let res = await fetch("http://localhost:5000/api/v1/products/create", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams(
-      new FormData(document.querySelector("#productForm"))
-    ),
-  });
+  let res = await fetch(
+    "https://lovely-worm-pea-coat.cyclic.app/api/v1/products/create",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams(
+        new FormData(document.querySelector("#productForm"))
+      ),
+    }
+  );
   // console.log(res);
   if (res.status === 201) {
     alert("Products added successfully");
